da3240b1dd872fe3c61df396230b96605734f3f1
Timothy D Meadows II <timothy@timothymeadows.com>
2017-10-17 13:02:52 -0700

    Move to version 1.0.0.

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var https = require('https');
 var querystring = require('querystring');
 var httpProxy = require('http-proxy');
+var url = require('url');
 var fs = require('fs');
 
 var config = JSON.parse(fs.readFileSync('config.json'));
@@ -20,7 +21,26 @@ var proxy = httpProxy.createProxyServer({
 });
 
 proxy.on('proxyReq', function (proxyReq, req, res, options) {
+    var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress || req.socket.remoteAddress ||req.connection.socket.remoteAddress;
+    var uri = url.parse(req.url);
+    var query = querystring.parse(uri.query) || {};
+    var host = req.headers["X-Host"] || req.headers["x-host"] || query["X-Host"] || query["x-host"];
+    var domain = url.parse(`https://${host}`);
+    
     proxyReq.removeHeader('X-Host');
+    if (proxyReq.path.indexOf("?")) {
+        if (query["x-host"] || query["X-Host"]) {
+            if (query["x-host"])
+                proxyReq.path = proxyReq.path.replace(`x-host=${query["x-host"]}`, "");
+            else
+                proxyReq.path = proxyReq.path.replace(`X-Host=${query["X-Host"]}`, "");         
+        }
+    }
+
+    if (proxyReq.path.substr(proxyReq.path.length - 1, 1) === "?")
+        proxyReq.path = proxyReq.path.substr(0, proxyReq.path.length - 2);
+    
+    console.log(`> [${ip}]:[${domain.hostname}] ${proxyReq.path}`);
 });
 
 var options = {
@@ -31,8 +51,13 @@ var options = {
 };
 
 var server = https.createServer(options, function (req, res) {
-    var query = querystring.parse(req.url);
-    var host = req.headers["X-Host"] || query["X-Host"];
+    var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress || req.socket.remoteAddress ||req.connection.socket.remoteAddress;
+    var uri = url.parse(req.url);
+    var query = querystring.parse(uri.query) || {};
+    var host = req.headers["X-Host"] || req.headers["x-host"] || query["X-Host"] || query["x-host"];
+    var domain = url.parse(`https://${host}`);
+
+    console.log(`< [${ip}]:[${domain.hostname}] ${req.url}`);
     if (!host) {
         res.statusCode = 400;
         res.statusMessage = "Missing X-Host parameter request was terminated.";
@@ -41,7 +66,6 @@ var server = https.createServer(options, function (req, res) {
         return;
     }
 
-    // TODO: should prob strip the x-host querystring key/value pair so it's not passed to the origin server
     proxy.web(req, res, {
         target: `https://${host}/`,
         autoRewrite: true,
@@ -54,3 +78,4 @@ var server = https.createServer(options, function (req, res) {
 });
 
 server.listen(config.port);
+console.log(`$ ${config.port}`);
