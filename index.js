b33abb91e6f594e9e487b3ae4288e16ff4086966
Timothy D Meadows II <timothy@timothymeadows.com>
2017-10-17 11:09:55 -0700

    Update shell script for linux, and add config.json

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 var https = require('https');
+var querystring = require('querystring');
 var httpProxy = require('http-proxy');
 var fs = require('fs');
+
+var config = fs.readFileSync('config.json');
+if (!config) {
+    console.log("No configuration file found! Please run 'configure' or 'configure.bat' first.");
+    return;
+}
 
 var proxy = httpProxy.createProxyServer({
     autoRewrite: true,
     changeOrigin: true,
     ssl: {
         pfx: fs.readFileSync('public-cert.pfx'),
-        passphrase: "59351!"
+        passphrase: config.passphrase
     },
     secure: true
 });
@@ -18,21 +25,23 @@ proxy.on('proxyReq', function (proxyReq, req, res, options) {
 
 var options = {
     pfx: fs.readFileSync('public-cert.pfx'),
-    passphrase: "59351!",
+    passphrase: config.passphrase,
     ciphers: 'ECDHE-RSA-AES256-SHA:AES256-SHA:RC4-SHA:RC4:HIGH:!MD5:!aNULL:!EDH:!AESGCM',
     honorCipherOrder: true
 };
 
 var server = https.createServer(options, function (req, res) {
-    var host = req.headers["X-Host"];
+    var query = querystring.parse(req.url);
+    var host = req.headers["X-Host"] || query["X-Host"];
     if (!host) {
         res.statusCode = 400;
-        res.statusMessage = "Missing X-Host header request was terminated.";
-        res.write("Missing X-Host header request was terminated.");
+        res.statusMessage = "Missing X-Host parameter request was terminated.";
+        res.write("Missing X-Host parameter request was terminated.");
         res.end();
         return;
     }
 
+    // TODO: should prob strip the x-host querystring key/value pair so it's not passed to the origin server
     proxy.web(req, res, {
         target: `https://${host}/`,
         autoRewrite: true,
@@ -44,4 +53,4 @@ var server = https.createServer(options, function (req, res) {
     });
 });
 
-server.listen(3232);
+server.listen(config.port);
