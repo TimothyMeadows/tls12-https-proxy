ff22afb2d4ea1115e22b21acfd8a8819bbc90524
Timothy D Meadows II <timothy@timothymeadows.com>
2017-10-20 09:57:01 -0700

    Bug fixes for 1.0.0

--- server.js
+++ server.js
@@ -13,34 +13,38 @@ if (!config || !config.port) {
 var proxy = httpProxy.createProxyServer({
     autoRewrite: true,
     changeOrigin: true,
+    hostRewrite: true,
     ssl: {
         pfx: fs.readFileSync('public-cert.pfx'),
-        passphrase: config.passphrase
+        passphrase: config.passphrase,
+        ciphers: 'RC4-SHA:RC4:ECDHE-RSA-AES256-SHA:AES256-SHA:HIGH:!MD5:!aNULL:!EDH:!AESGCM',
+        honorCipherOrder: true
     },
     secure: true
 });
 
 proxy.on('proxyReq', function (proxyReq, req, res, options) {
-    var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress || req.socket.remoteAddress ||req.connection.socket.remoteAddress;
+    var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress || req.socket.remoteAddress || req.connection.socket.remoteAddress;
     var uri = url.parse(req.url);
     var query = querystring.parse(uri.query) || {};
     var host = req.headers["X-Host"] || req.headers["x-host"] || query["X-Host"] || query["x-host"];
     var domain = url.parse(`https://${host}`);
-    
+
     proxyReq.removeHeader('X-Host');
     if (proxyReq.path.indexOf("?")) {
         if (query["x-host"] || query["X-Host"]) {
             if (query["x-host"])
                 proxyReq.path = proxyReq.path.replace(`x-host=${query["x-host"]}`, "");
             else
-                proxyReq.path = proxyReq.path.replace(`X-Host=${query["X-Host"]}`, "");         
+                proxyReq.path = proxyReq.path.replace(`X-Host=${query["X-Host"]}`, "");
         }
     }
 
     if (proxyReq.path.substr(proxyReq.path.length - 1, 1) === "?")
-        proxyReq.path = proxyReq.path.substr(0, proxyReq.path.length - 2);
-    
-    console.log(`> [${ip}]:[${domain.hostname}] ${proxyReq.path}`);
+        proxyReq.path = proxyReq.path.substr(0, proxyReq.path.length - 1);
+
+    req.url = proxyReq.path;
+    console.log(`[${ip}]:[${domain.hostname}] ${proxyReq.path}`);
 });
 
 var options = {
@@ -50,30 +54,40 @@ var options = {
     honorCipherOrder: true
 };
 
+var terminate = function(res) {
+    res.statusCode = 400;
+    res.statusMessage = "Missing X-Host parameter request was terminated.";
+    res.write("Missing X-Host parameter request was terminated.");
+    res.end();
+}
+
 var server = https.createServer(options, function (req, res) {
-    var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress || req.socket.remoteAddress ||req.connection.socket.remoteAddress;
+    var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress || req.socket.remoteAddress || req.connection.socket.remoteAddress;
     var uri = url.parse(req.url);
     var query = querystring.parse(uri.query) || {};
     var host = req.headers["X-Host"] || req.headers["x-host"] || query["X-Host"] || query["x-host"];
-    var domain = url.parse(`https://${host}`);
 
-    console.log(`< [${ip}]:[${domain.hostname}] ${req.url}`);
     if (!host) {
-        res.statusCode = 400;
-        res.statusMessage = "Missing X-Host parameter request was terminated.";
-        res.write("Missing X-Host parameter request was terminated.");
-        res.end();
-        return;
+        var referer = req.headers["referer"];
+        if (referer) {
+            var refererUri = url.parse(referer);
+            var refererQuery = querystring.parse(refererUri.query) || {};
+
+            host = refererQuery["X-Host"] || refererQuery["x-host"];
+            if (!host) {
+                terminate(res);
+                return;
+            }
+
+            host = url.parse(`https://${host}`).hostname;
+        } else {
+            terminate(res);
+            return;
+        }
     }
 
     proxy.web(req, res, {
-        target: `https://${host}/`,
-        autoRewrite: true,
-        changeOrigin: true,
-        ssl: {
-            ciphers: 'RC4-SHA:RC4:ECDHE-RSA-AES256-SHA:AES256-SHA:HIGH:!MD5:!aNULL:!EDH:!AESGCM',
-            honorCipherOrder: true
-        }
+        target: `https://${host}`
     });
 });
 
