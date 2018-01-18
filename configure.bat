1f16587c1fe6d39a68e2133b5c5dd2bd992dbaad
Timothy D Meadows II <timothy@timothymeadows.com>
2018-01-18 08:53:47 -0800

    update config scripts to add verbose and first key.

--- configure.bat
+++ configure.bat
@@ -3,24 +3,30 @@ GOTO passphrase
 
 :passphrase
 ECHO The proxy will need the password for the certificate you are using.
+ECHO.
 SET /P passphrase=Passphrase: 
+ECHO.
 GOTO port
 
 :port
 ECHO What port would you like the proxy to run on?
 ECHO Note: Any port below 1024 will require admin!
+ECHO.
 SET /P port=Port: 
+ECHO.
+GOTO key
+
+:key
+ECHO Please enter a key you would like to use. More can be entered into the config.json file later.
+ECHO.
+SET /P key=Key: 
+ECHO.
 GOTO save
 
 :save
-ECHO { \"port\": \"%port%\", \"passphrase\": \"%passphrase%\" } > config.json
+ECHO { "port": "%port%", "passphrase": "%passphrase%", "verbose": true, "keys": ["%key%"] } > config.json
 GOTO install
 
 :install
 npm install
-GOTO end
-
-:end
-ECHO Configuration done. You can start the proxy with 'npm start' or 'node index'
-PAUSE
 @ECHO ON
