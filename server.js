3d12d126bd44f3c13b7d7ed5df8eca94c133370f
Timothy D Meadows II <timothy@timothymeadows.com>
2018-01-18 08:33:35 -0800

    add basic key system

--- server.js
+++ server.js
@@ -17,10 +17,23 @@ var options = {
     honorCipherOrder: true
 };
 
-var terminate = function (res) {
+var terminate = function (res, ip) {
+    if (config.verbose && config.verbose === true)
+        console.log(`[${now()}] ${ip} terminated`);
+
     res.statusCode = 400;
     res.statusMessage = "Missing X-Host parameter request was terminated.";
     res.write("Missing X-Host parameter request was terminated.");
+    res.end();
+};
+
+var denied = function (res, ip) {
+    if (config.verbose && config.verbose === true)
+        console.log(`[${now()}] ${ip} access denied`);
+
+    res.statusCode = 403;
+    res.statusMessage = "Access denied.";
+    res.write("Access denied.");
     res.end();
 };
 
@@ -30,10 +43,13 @@ var rewrite = function (headers, host) {
         if (key === "x-host" || key === "X-Host")
             continue;
 
+        if (key === "x-key" || key === "X-Key")
+            continue;
+
         // TODO: Remove when gzip is supported
         if (key === "Accept" || key === "accept")
             headers[key] = "text/html,application/xhtml+xml,application/xml,application/json;";
-        
+
         // TODO: Remove when gzip is supported
         if (key === "Accept-Encoding" || key === "accept-encoding")
             continue;
@@ -49,11 +65,27 @@ var rewrite = function (headers, host) {
     return headerString;
 };
 
+var now = function () {
+    var date = new Date();
+    return `${(((date.getMonth() +1) < 10) ? "0" : "") + (date.getMonth() +1)}/${((date.getDate() < 10) ? "0" : "") + date.getDate()}/${date.getFullYear()} ${(date.getHours() < 10 ? "0" : "") + date.getHours()}:${((date.getMinutes() < 10) ? "0" : "") + date.getMinutes()}:${((date.getSeconds() < 10) ? "0" : "") + date.getSeconds()}`;
+}
+
 var server = https.createServer(options, function (req, res) {
     var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress || req.socket.remoteAddress || req.connection.socket.remoteAddress;
     var uri = url.parse(req.url);
     var query = querystring.parse(uri.query) || {};
     var host = req.headers["X-Host"] || req.headers["x-host"] || query["X-Host"] || query["x-host"];
+    var key = req.headers["X-Key"] || req.headers["x-key"] || query["X-Key"] || query["x-key"];
+
+    if (!key) {
+        denied(res, ip);
+        return;
+    }
+
+    if (config.keys.indexOf(key) === -1) {
+        denied(res, ip);
+        return;
+    }
 
     if (!host) {
         var referer = req.headers["referer"];
@@ -63,19 +95,19 @@ var server = https.createServer(options, function (req, res) {
 
             host = refererQuery["X-Host"] || refererQuery["x-host"];
             if (!host) {
-                terminate(res);
+                terminate(res, ip);
                 return;
             }
 
             host = url.parse(`https://${host}`).hostname;
         } else {
-            terminate(res);
+            terminate(res, ip);
             return;
         }
     }
 
     if (config.verbose && config.verbose === true)
-        console.log(`${ip} -> ${host}`);
+        console.log(`[${now()}] ${ip} -> ${host}`);
 
     var socket = tls.connect(443, url.parse(`https://${host}`).hostname, () => {
         var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress || req.socket.remoteAddress || req.connection.socket.remoteAddress;
@@ -98,7 +130,7 @@ var server = https.createServer(options, function (req, res) {
             var payload = `${req.method} ${host_uri.path} HTTP/1.0\r\n${rewrite(req.headers, host_uri.hostname)}\r\n\r\n`;
             socket.write(payload);
         }
-        
+
         process.stdin.pipe(socket);
         process.stdin.resume();
     });
@@ -113,9 +145,9 @@ var server = https.createServer(options, function (req, res) {
         socket.destroy();
     });
 
-    setTimeout(function() {
+    setTimeout(function () {
         if (config.verbose && config.verbose === true)
-            console.log(`${ip} <- ${host}`);
+            console.log(`[${now()}] ${ip} <- ${host}`);
 
         var packet = response.split("\r\n\r\n");
         var headers = packet[0].split("\r\n");
